428afeb878d7cfa9c71d24859c0b93df0b913fd9
davinkhor <73937826+deltaMinor@users.noreply.github.com>
2026-01-24 17:19:38 +0800

    Add greeting

--- src/main/java/HERM35.java
+++ src/main/java/HERM35.java
@@ -2,5 +2,8 @@ public class HERM35 {
     public static void main(String[] args) {
         String name = "HERM35";
         System.out.println("Hey! I'm " + name + "!");
+        System.out.println("What can I do for you?");
+        System.out.println("-----------------------");
+        System.out.println("Bye, see you later!");
     }
 }
