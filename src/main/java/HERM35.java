ab3779d4120c8e0d0c9dd76f12aebe855c87b6ad
davinkhor <73937826+deltaMinor@users.noreply.github.com>
2026-01-24 21:25:34 +0800

    Add mark and unmark tasks function

--- src/main/java/HERM35.java
+++ src/main/java/HERM35.java
@@ -15,6 +15,23 @@ public class HERM35 {
         printMessage(introduction);
         while (input.hasNextLine()) {
             command = input.nextLine();
+            String words[] = command.split(" ");
+            if (words.length == 2) {
+                if (isInteger(words[1])) {
+                    int taskIndex = Integer.parseInt(words[1]) - 1;
+                    if (taskIndex < taskListCount && taskIndex >= 0) {
+                        if (words[0].equals("mark")) {
+                            taskList[taskIndex].mark(true);
+                            printMessage("Sure, I've marked this task as done:\n" + taskList[taskIndex]);
+                            continue;
+                        } else if (words[0].equals("unmark")) {
+                            taskList[taskIndex].mark(false);
+                            printMessage("OK, I've marked this task as not done:\n" + taskList[taskIndex]);
+                            continue;
+                        }
+                    }
+                }
+            }
             switch(command) {
                 case "list":
                     String listOutput = "";
@@ -50,5 +67,17 @@ public class HERM35 {
 
     public static void exit() {
         printMessage("Bye, see you later!");
+    }
+
+    public static boolean isInteger(String str) {
+        if (str == null) {
+            return false;
+        }
+        try {
+            double i = Integer.parseInt(str);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
     }
 }
