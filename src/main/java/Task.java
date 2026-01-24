c7bbb489ca6e4997f052e2251698b2ff021882a8
davinkhor <73937826+deltaMinor@users.noreply.github.com>
2026-01-24 20:36:04 +0800

    Add Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,13 @@
+public class Task {
+    private Boolean done;
+    private String name;
+
+    public Task(String name) {
+        done = false;
+        this.name = name;
+    }
+
+    public void mark(Boolean done) {
+        this.done = done;
+    }
+}
