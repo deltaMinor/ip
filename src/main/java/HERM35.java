1936d5800e264f6d844601e01bf5485d4f9d7f6e
davinkhor <73937826+deltaMinor@users.noreply.github.com>
2026-01-24 18:01:26 +0800

    Add printMessage() and exit() method
    Add printMessage(String message) method to print message with
    formatting
    
    Refactor exit procedure to utilize printMessage() method

--- src/main/java/HERM35.java
+++ src/main/java/HERM35.java
@@ -1,9 +1,19 @@
 public class HERM35 {
+
     public static void main(String[] args) {
         String name = "HERM35";
         System.out.println("Hey! I'm " + name + "!");
         System.out.println("What can I do for you?");
+        exit();
+    }
+
+    public static void printMessage(String message) {
         System.out.println("-----------------------");
-        System.out.println("Bye, see you later!");
+        System.out.println(message);
+        System.out.println("-----------------------");
+    }
+
+    public static void exit() {
+        printMessage("Bye, see you later!");
     }
 }
