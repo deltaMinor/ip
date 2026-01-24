497a12ca2d3e9a485d082fd0902b0ccaf30a4b6c
davinkhor <73937826+deltaMinor@users.noreply.github.com>
2026-01-24 18:16:36 +0800

    Add echo and input support

--- src/main/java/HERM35.java
+++ src/main/java/HERM35.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
+
 public class HERM35 {
 
+    private static String command;
+
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         String name = "HERM35";
         System.out.println("Hey! I'm " + name + "!");
         System.out.println("What can I do for you?");
-        exit();
+        while (input.hasNextLine()) {
+            command = input.nextLine();
+            if (command.equals("bye")) {
+                exit();
+            } else {
+                printMessage(command);
+            }
+        }
     }
 
     public static void printMessage(String message) {
