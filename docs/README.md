3a160d3d72a6a9a9106ce9419b860126738f3754
davinkhor <73937826+deltaMinor@users.noreply.github.com>
2026-01-24 16:36:08 +0800

    Rename main class

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# HERM35 User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/HERM35.java
@@ -0,0 +1,6 @@
+public class HERM35 {
+    public static void main(String[] args) {
+        String name = "HERM35";
+        System.out.println("Hey! I'm " + name + "!");
+    }
+}
