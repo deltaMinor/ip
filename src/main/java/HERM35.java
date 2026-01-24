c58ebd373e6109e5d7e6b3987a317014c8ef8878
davinkhor <73937826+deltaMinor@users.noreply.github.com>
2026-01-24 19:28:00 +0800

    bug fix: Fix bug in echo
    HERM35 does not finish after receiving "bye" input
    
    Implement end loop when "bye" input is received
    
    Add printLine() method to maintain consistency of message formatting
    
    Refactor printMessage() method

--- src/main/java/HERM35.java
+++ src/main/java/HERM35.java
@@ -3,16 +3,18 @@ import java.util.Scanner;
 public class HERM35 {
 
     private static String command;
+    private static final String LINE_SEPARATOR = "-----------------------";
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String name = "HERM35";
-        System.out.println("Hey! I'm " + name + "!");
-        System.out.println("What can I do for you?");
+        String introduction = "Hey! I'm " + name + "!\nWhat can I do for you?";
+        printMessage(introduction);
         while (input.hasNextLine()) {
             command = input.nextLine();
             if (command.equals("bye")) {
                 exit();
+                break;
             } else {
                 printMessage(command);
             }
@@ -20,9 +22,16 @@ public class HERM35 {
     }
 
     public static void printMessage(String message) {
-        System.out.println("-----------------------");
-        System.out.println(message);
-        System.out.println("-----------------------");
+        printLine(LINE_SEPARATOR);
+        Scanner reader = new Scanner(message);
+        while (reader.hasNextLine()) {
+            printLine(reader.nextLine());
+        }
+        printLine(LINE_SEPARATOR);
+    }
+
+    public static void printLine(String line) {
+        System.out.println("\t" + line);
     }
 
     public static void exit() {
