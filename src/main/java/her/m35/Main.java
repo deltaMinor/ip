9228fd9ec7085d7ce35d165532f9d2fd64e5d5c4
davinkhor <73937826+deltaMinor@users.noreply.github.com>
2026-02-14 01:29:44 +0800

    Update window icon and title

--- src/main/java/her/m35/Main.java
+++ src/main/java/her/m35/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -15,6 +16,8 @@ public class Main extends Application {
 
     private Herm35 herm35 = new Herm35();
 
+    private Image icon = new Image(this.getClass().getResourceAsStream("/images/HERM35Icon.png"));
+
     @Override
     public void start(Stage stage) {
         try {
@@ -22,6 +25,8 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("HERM35");
+            stage.getIcons().add(icon);
 
             stage.setMinHeight(320);
             stage.setMinWidth(417);
